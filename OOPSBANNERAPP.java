d74c46929d0aef4f7f7a83e2b110033f38a2cd94
varsharajeesh10-hue <varsharajeesh10@gmail.com>
2026-02-16 21:08:03 +0300

    Add files via upload

---
+++ OOPSBANNERAPP.java
@@ -0,0 +1,16 @@
+class OOPSBANNERAPP {
+
+    void printOops() {
+        System.out.println("OOOOO  OOOOO  PPPPP  SSSSS");
+        System.out.println("O   O  O   O  P   P  S    ");
+        System.out.println("O   O  O   O  PPPPP  SSSSS");
+        System.out.println("O   O  O   O  P      S    ");
+        System.out.println("OOOOO  OOOOO  P      SSSSS");
+    }
+
+    public static void main(String[] args) {
+        OOPSBANNERAPP obj=new 
+		OOPSBANNERAPP();//object creation
+        obj.printOops(); // method call
+    }
+}
