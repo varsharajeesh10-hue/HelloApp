b559e7e9d11fdbe7b3c9f75068f6db8666a9c7df
varsharajeesh10-hue <varsharajeesh10@gmail.com>
2026-02-22 23:32:29 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,35 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                " *** ",
+                " *** ",
+                "*****",
+                "*****"));
+
+        System.out.println(String.join("   ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**     "));
+
+        System.out.println(String.join("   ",
+                "**   **",
+                "**   **",
+                "***** ",
+                " ***  "));
+
+        System.out.println(String.join("   ",
+                "**   **",
+                "**   **",
+                "**     ",
+                "     **"));
+
+        System.out.println(String.join("   ",
+                " *** ",
+                " *** ",
+                "**   ",
+                "*****"));
+    }
+}
